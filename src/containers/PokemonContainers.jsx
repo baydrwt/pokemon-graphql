56a2135431e0b92d9690e8485672cdd22f01c2d3
baydrwt <bayu0825.bd@gmail.com>
2024-09-09 07:53:48 +0700

    learn-GraphQL

--- src/containers/PokemonContainers.jsx
+++ src/containers/PokemonContainers.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import Pokemon from "../components/Pokemon";
+import { GET_POKEMONS } from "../graphql/get-pokemons";
 
 export function PokemonContainer() {
-  return <p>Im a Pokemon Container</p>;
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 12 },
+  });
+
+  return <div className="container">{pokemons && pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon} />)}</div>;
 }
